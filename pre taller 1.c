f5f74a0939e7e693fc748012874b04173d619c47
Miguel <41808207+Mikol1407@users.noreply.github.com>
2023-05-21 18:18:48 -0400

    Update pre taller 1.c
    Terminado sin testear

--- pre taller 1.c
+++ pre taller 1.c
@@ -14,11 +14,11 @@ int main(){
     cimpares = 0;
     ceros = 0;
     j = 2;
-    band = 
+    band = 0
     printf ("ingrese la cantidad de numeros a evaluar \n");
     scanf ("%d", &n);
     for (i = 1; i == n; i++){
-        printf ("ingrese el numero \"%d\"", i+1);
+        printf ("ingrese el numero \"%d\"\n", i+1);
         scanf ("%d", num);
         if (num % 2 == 0){
             cpares =cpares + 1;
@@ -28,6 +28,24 @@ int main(){
         if (num == 0){
             ceros = ceros +1;
         }
-        
+        if (num > 2){
+            do {
+                band = num % j;
+                j = j + 1;
+            }while (j < n && band =0);
+            if (band = 0){
+                cprimos = cprimos + 1;
+            }
+        }else {
+            if (num == 2){
+                cprimos = cprimos +1;
+            }
+        }
     }
+    printf ("Cantidad de pares: %d\n",cpares);
+    printf ("Cantidad de impares: %d\n", cimpares);
+    printf ("Cantidad de ceros: %d\n", ceros);
+    printf ("Cantidad de primos: %d\n", cprimos);
+    getch ();
+    return 0;
 }
