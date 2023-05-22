9b5de3ee3bbea86ed5e51beecfd765ef62e12175
Miguel <41808207+Mikol1407@users.noreply.github.com>
2023-05-21 20:16:45 -0400

    Update pre taller 1.c
    Contador de primos mejorado, aún sin funcionar correctamente

--- pre taller 1.c
+++ pre taller 1.c
@@ -7,19 +7,18 @@ ser introducido. Calcular: 1) Cuántos son pares e impares, 2) Cuántos ceros se
 
 int main(){
 
-    int n, num, cprimos, cpares, cimpares, ceros, i, j, band;
+    int n, num, cprimos, cpares, cimpares, ceros, i, j, band, cont;
 
     cprimos = 0;
     cpares = 0;
     cimpares = 0;
     ceros = 0;
-    j = 2;
-    band = 0
     printf ("ingrese la cantidad de numeros a evaluar \n");
     scanf ("%d", &n);
-    for (i = 1; i == n; i++){
-        printf ("ingrese el numero \"%d\"\n", i+1);
-        scanf ("%d", num);
+    for (i = 1; i <= n; i++){
+        cont = 0;
+        printf ("ingrese el numero \"%d\"\n", i);
+        scanf ("%d", &num);
         if (num % 2 == 0){
             cpares =cpares + 1;
         } else {
@@ -28,16 +27,12 @@ int main(){
         if (num == 0){
             ceros = ceros +1;
         }
-        if (num > 2){
-            do {
-                band = num % j;
-                j = j + 1;
-            }while (j < n && band =0);
-            if (band = 0){
-                cprimos = cprimos + 1;
+        for (j = 1; j <= num; j++){
+            band = num % j;
+            if (band == 0){
+                cont = cont + 1;
             }
-        }else {
-            if (num == 2){
+            if (cont == 2){
                 cprimos = cprimos +1;
             }
         }
