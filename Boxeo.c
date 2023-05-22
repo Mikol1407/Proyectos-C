ea50074b576e5a4c577b9085c93718755c5a6b30
Miguel <41808207+Mikol1407@users.noreply.github.com>
2023-05-22 18:31:10 -0400

    Update Boxeo.c
    Cambiada la func juez, iniciado el main

--- Boxeo.c
+++ Boxeo.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
-void jueces (int *puntfinr, int *puntfina);
+void juez (int *puntfinr, int *puntfina, int i, int *roundr, int *rounda );
+
+int main (){
+    
+    int ptsrojo, ptsazul, i,
+}
 
 
 
 
 
-
-
-void jueces (int *puntfinr, int *puntfina){
-    int i, j, ra, contr = 0, conta = 0, caidar,caidaa, puntosr, puntosa, totalpr = 0, totalpa = 0;
-    for (i = 1; i < 4; i++){
+void juez (int *puntfinr, int *puntfina, int i, int *roundr, int *rounda){
+    
+    int j, rg, contr = 0, conta = 0, caidar,caidaa, puntosr, puntosa;
         printf ("Puntuaciones Juez #%d\n", i);
         for (j = 1; j < 13; j++){
             do {
-                printf ("Quien gana el round #%d?\nBoxeador rojo = 1\n Boxeador azul = 0", j);
-                scanf ("%d", &ra);
-            } while (ra != 0 || ra != 1);
+                printf ("Quien gana el round #%d?\nBoxeador rojo = 1\nBoxeador azul = 0\n", j);
+                scanf ("%d", &rg);
+            } while (rg < 0 || rg > 1);
             do {
-                printf ("El Boxeador rojo cayo al suelo?\nSi=1\nNo=0");
+                printf ("El Boxeador rojo cayo al suelo?\nSi=1\nNo=0\n");
                 scanf ("%d", &caidar);
-            } while (caidar != 1 || caidar != 0);
+            } while (caidar > 1 || caidar < 0);
             do{
-                printf ("El boxeador azul cayo al suelo?\nSi=1\nNo=0");
+                printf ("El boxeador azul cayo al suelo?\nSi=1\nNo=0\n");
                 scanf ("%d", &caidaa);
-            } while (caidaa != 1 || caidaa != 0);
-            if (ra == 1){
+            } while (caidaa > 1 || caidaa < 0);
+            if (rg == 1){
                 contr ++;
                 puntosr = 10;
                 puntosa = 9;
@@ -40,18 +43,21 @@ void jueces (int *puntfinr, int *puntfina){
             if (caidaa == 1){
                 puntosa = puntosa - 2
             }
-            if (ra == 1){
+            if (rg == 1){
                 printf ("El boxeador rojo gana el round #%d\n", j);
             } else {
                 printf ("El boxeador azul gana el round #%d\n", j);
             }
             printf ("*PUNTUACION DEL ROUND*\nRojo=%d\tAzul%d\n", puntosr, puntosa);
-            totalpr = puntosr + totalpr;
-            totalpa = puntosa + totalpa;
+            *puntfinr = puntosr + *puntfinr;
+            *puntfina = puntosa + *puntfina;
         }
-        printf ("Puntuaciones totales del juez #%d\n", j);
-        printf ("Puntos Rojo: %d", totalpr);
-        printf ("Puntos Azul: %d", totalpa);
-        
-    }
+    printf ("Puntuaciones totales del juez #%d\n", i);
+    printf ("Puntos Rojo: %d", *puntfinr);
+    printf ("---Puntos Azul: %d\n", *puntfinr);
+    printf ("El Boxeador rojo gana #%d rounds\n", contr);
+    printf ("El Boxeador azul gana #%d rounds\n", conta);
+    *roundr = contr;
+    *rounda = conta;
+
 }
